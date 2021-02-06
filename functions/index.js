595b38b696862f5452fd13c28c1b4446d0c90f78
Mayar-Elabbasy <mayarelabbasy2016@gmail.com>
2021-02-06 23:56:19 +0200

    added /payments/create endpoint to complete payment using stripe

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,27 @@
 const functions = require("firebase-functions");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const express = require('express');
+const cors = require('cors');
+const stripe = require('stripe')('sk_test_51IFOIjEE9KsFcKposrNEkMpP5SQakfyxMcLgmDA0UhBWZFIIkTjYFXhiSkWuFOP8O1DWznTSYM4PWxMo3tlQA8Jn00khDMdaLW');
+
+const app = express();
+
+app.use(cors({ origin: true }));
+app.use(express.json());
+
+app.get('/', (request, response) => response.status(200).send('Hello Mayar'))
+
+app.post('/payments/create', async (request, response) => {
+    const total = request.query.total;
+    console.log("================total===========: ", total);
+    const paymentIntent = await stripe.paymentIntents.create({
+        amount: total,
+        currency: 'usd'
+    })
+    response.status(201).send({
+        clientSecret: paymentIntent.client_secret,      
+    })
+})
+
+
+exports.api = functions.https.onRequest(app);
